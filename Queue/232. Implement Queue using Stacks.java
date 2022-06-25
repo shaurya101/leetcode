068a2e71fedbcbff820be4c18e20e3899b55f2b1
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2022-06-26 00:53:47 +0530

    Stack, Queue, Design

---
+++ Queue/232. Implement Queue using Stacks.java
@@ -0,0 +1,88 @@
+/*
+232. Implement Queue using Stacks
+
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+*/
+---------------------
+
+class MyQueue {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public MyQueue() {
+    }
+    
+    public void push(int x) {
+        int n=s1.size();
+        // moving s1 to s2
+        while(n>0){
+            s2.push(s1.pop());
+            n--;
+        }
+        // adding the new element in s1
+        s1.push(x);
+        // moving s2 back to s1
+        while(!s2.isEmpty())
+            s1.push(s2.pop());
+    }
+    
+    public int pop() {
+        return s1.pop();
+    }
+    
+    public int peek() {
+        return s1.peek();
+    }
+    
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+// Approach 1 - Using costly push operation.
+// We will first copy our elements from s1 to s2.
+// We will push the new element in s1.
+// Then we will push s2 elements into s1.
+
+// Approach 2 - Using costly pop and top operations
+// Push operation will be as usual
+// in pop; we will first pop all the elements except last from s1 to s2
+// we will store this last element in x, and meanwhile we will copy all elements of s2 back to s1. And return x.
+// In top; We will copy all elements from s1 to s2. Store last element of s2 in x.
+// Move back elements from s2 to s1. Return x.
+
+// Approach 1 more efficient than approach 2
