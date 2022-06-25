3c8842d47d3a2b5c41c8bc46b896353cbf65a7c2
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2022-06-26 03:18:42 +0530

    Stack, Queue, Design

--- Queue/232. Implement Queue using Stacks.java
+++ Queue/232. Implement Queue using Stacks.java
@@ -28,6 +28,48 @@ myQueue.peek(); // return 1
 myQueue.pop(); // return 1, queue is [2]
 myQueue.empty(); // return false
 */
+
+---------------------
+    
+ 
+// Approach 1 - Two times stack gives a queue as order is reversed twice gives initial order
+// There is one input stack, onto which we push the incoming elements, and one output stack, from which we peek/pop. 
+// We move elements from input stack to output stack when needed, i.e., when I need to peek/pop but the output stack is empty. When that happens, I move all elements from input to output stack, thereby reversing the order so it's the correct order for peek/pop.
+// The loop in peek does the moving from input to output stack. Each element only ever gets moved like that once, though, and only after we already spent time pushing it, so the overall amortized cost for each operation is O(1).
+    
+    
+ class MyQueue {
+    Stack<Integer> input = new Stack<>();
+    Stack<Integer> output = new Stack<>();
+
+    public MyQueue() {
+    }
+    
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    public int pop() {
+        peek(); // NOTE- 3 things to clear. This is not a syntax error.
+        // 1. It calls the class method. 
+        // 2. We may think there is a return statement in peek() but here we do not store the returned value in a variable. But it works. It is not necessary to 'catch' the returned value from the called function.
+        // 3. Also class method peek() is not static so does it not need an object? Or is it because we are calling it from inside class we dont need to mention object?
+        return output.pop(); // stack method pop 
+    }
+    
+    public int peek() {
+        if(output.isEmpty())
+            while(!input.isEmpty())
+                output.push(input.pop());
+        return output.peek(); // stack method peek
+    }
+    
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
+    
+    
 ---------------------
 
 class MyQueue {
@@ -73,16 +115,16 @@ class MyQueue {
  * boolean param_4 = obj.empty();
  */
 
-// Approach 1 - Using costly push operation.
+// Approach 2 - Using costly push operation.
 // We will first copy our elements from s1 to s2.
 // We will push the new element in s1.
 // Then we will push s2 elements into s1.
 
-// Approach 2 - Using costly pop and top operations
+// Approach 3 - Using costly pop and top operations
 // Push operation will be as usual
 // in pop; we will first pop all the elements except last from s1 to s2
 // we will store this last element in x, and meanwhile we will copy all elements of s2 back to s1. And return x.
 // In top; We will copy all elements from s1 to s2. Store last element of s2 in x.
 // Move back elements from s2 to s1. Return x.
 
-// Approach 1 more efficient than approach 2
+// Approach 2 and 3 are inefficient.
