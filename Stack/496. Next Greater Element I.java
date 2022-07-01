edb9781e2133318b1524712a049b4f21409ab02e
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2022-07-01 13:33:49 +0530

    Stack, HashMap, Array

--- Stack/496. Next Greater Element I.java
+++ Stack/496. Next Greater Element I.java
@@ -58,6 +58,13 @@ class Solution {
 // We finally push the nums2[i] in stack as we would want to find its next greater element as well.
 // If greater, we add it to the hashmap.
 
+// Key observation:
+// Suppose we have a decreasing sequence followed by a greater number
+// For example [5, 4, 3, 2, 1, 6] then the greater number 6 is the next greater element for all previous numbers in the sequence
+// We use a stack to keep a decreasing sub-sequence, whenever we see a number x greater than stack.peek() we pop all elements less than x and for all the popped ones, their next greater element is x
+// For example [9, 8, 7, 3, 2, 1, 6]
+// The stack will first contain [9, 8, 7, 3, 2, 1] and then we see 6 which is greater than 1 so we pop 1 2 3 whose next greater element should be 6
+
 
 // Finding greatest element to the right Approach B
 
