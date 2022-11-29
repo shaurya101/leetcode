2e60fb12cfe88c30cab511aaf02413ca6f3972c3
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2022-11-30 01:03:04 +0530

    Tree, Queue, Inorder traversal

---
+++ BinaryTree/897. Increasing Order Search Tree.java
@@ -0,0 +1,53 @@
+/*
+897. Increasing Order Search Tree
+
+Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+Example 1:
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+
+*/
+
+----------
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Queue<TreeNode> q1 = new LinkedList<>();
+    public TreeNode increasingBST(TreeNode root) {
+        if(root == null)
+            return null;
+        
+        inorder(root);
+        root = q1.poll();
+        TreeNode rootNew = root;
+        while(!q1.isEmpty()) {
+            root.left = null; // else root.left will be pointing to some node. Cycles may be created if root.left is not made null
+            root.right = q1.poll();
+            root = root.right;
+        }
+        root.left = null; // last node's left should be null as well
+        return rootNew;
+    }
+    private void inorder(TreeNode root) {
+        if(root == null)
+            return;
+        inorder(root.left);
+        q1.add(root);
+        inorder(root.right);
+    }
+}
