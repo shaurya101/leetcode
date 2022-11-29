8f883a5a7b2627fe6e7b83c72db76a031409d9c1
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2022-11-29 18:46:17 +0530

    Tree, recursion

--- BinaryTree/2331. Evaluate Boolean Binary Tree.java
+++ BinaryTree/2331. Evaluate Boolean Binary Tree.java
@@ -18,6 +18,36 @@ A leaf node is a node that has zero children.
 */
 
 ------------
+// Both approaches with recurion. This one is more simplified.
+    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root.val==0)
+            return false;
+        if(root.val==1)
+            return true;
+        
+        if(root.val==2) return evaluateTree(root.left) || evaluateTree(root.right);
+        return evaluateTree(root.left) && evaluateTree(root.right);
+    }
+}
+    
+------------
 
 /**
  * Definition for a binary tree node.
