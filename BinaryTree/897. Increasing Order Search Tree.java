5bc04f9e175f3237260d99913a5c6ceb2a52d318
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2022-11-30 02:34:12 +0530

    Tree, Recursion, Inbuilt stack, Queue

--- BinaryTree/897. Increasing Order Search Tree.java
+++ BinaryTree/897. Increasing Order Search Tree.java
@@ -30,21 +30,23 @@ Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
  */
 class Solution {
     public TreeNode increasingBST(TreeNode root) {
-        return reverseInOrder(root);
+        return newRoot(root);
     }
-    private TreeNode reverseInOrder(TreeNode root) {
+ 
+    private TreeNode newRoot(TreeNode root) {
         if(root == null)
-            return null;
-        root.right = reverseInOrder(root.right); // returns new root of right subtree which will be on right of root's node
-        TreeNode newRoot = reverseInOrder(root.left); // returns new head of left subtree which will be new head of this tree.
+            return null; 
+        
+        root.right = newRoot(root.right); // returns new root of right subtree which will be on right of root's node
+        TreeNode newLeftRoot = newRoot(root.left); // returns new head of left subtree which will be new head of this tree.
         // But also, we need to attach the root to the tail of the left subtree (ie attach the root to the rightmost node of left subtree)
         // Before this we also make root.left=null to avoid any cycles
         root.left = null; 
-        if(newRoot == null)
+        if(newLeftRoot == null)
             return root;
-        TreeNode tail = rightMost(newRoot);
+        TreeNode tail = rightMost(newLeftRoot);
         tail.right = root;
-        return newRoot;
+        return newLeftRoot;
     }
     private TreeNode rightMost(TreeNode root) {
         TreeNode prev = null;
@@ -54,7 +56,19 @@ class Solution {
         }
         return prev;
     }
-}   
+}
+
+// breakdown of recursive function
+// For a root, we have to find the newRoot of its right subtree and the newRoot of its left subtree. Also, for the left subtree we have to find its tail(ie the rightmost node in the subtree when traversing in InOrder). This tail will point to the root. The linking will be done and the new root of the tree will be newRoot of the left subtree.
+// in a tree, we have to change the pointers such that the root will point to its right subtree's 'newRoot'(which we will find by running the newRoot(root.right)) and the left subtree's tail(rightmost node in inOrder traversal) will point to root and the newRoot of this tree will be the newRoot of left subtree.
+
+
+// Though process
+// In a Binary tree
+// When thinking of recursion, consider the entire tree as three nodes only. The root, the left subtree and the right subtree. Now solve the problem for this three nodes. 
+// Only now, whatever we had to do with the left node we will be doing with the left subtree by calling the same function again(hence recursion) for left subtree where it will have its own root and left and right subtrees. 
+// Similarly for the right subtree.
+// This way we breakdown a huge tree into three nodes where we can easily think of the recursive conditions or requirements of the question.
  
 ----------
 
