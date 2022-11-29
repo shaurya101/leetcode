02e66dff26cff443b36c3f35328f813132f2dc01
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2022-11-30 02:09:44 +0530

    Tree, Inbuilt stack, Recursion, Queue

--- BinaryTree/897. Increasing Order Search Tree.java
+++ BinaryTree/897. Increasing Order Search Tree.java
@@ -11,6 +11,55 @@ Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
 
 ----------
 
+// Approach 1 - Recursion and using inbuilt OS recursive stack
+ 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        return reverseInOrder(root);
+    }
+    private TreeNode reverseInOrder(TreeNode root) {
+        if(root == null)
+            return null;
+        root.right = reverseInOrder(root.right); // returns new root of right subtree which will be on right of root's node
+        TreeNode newRoot = reverseInOrder(root.left); // returns new head of left subtree which will be new head of this tree.
+        // But also, we need to attach the root to the tail of the left subtree (ie attach the root to the rightmost node of left subtree)
+        // Before this we also make root.left=null to avoid any cycles
+        root.left = null; 
+        if(newRoot == null)
+            return root;
+        TreeNode tail = rightMost(newRoot);
+        tail.right = root;
+        return newRoot;
+    }
+    private TreeNode rightMost(TreeNode root) {
+        TreeNode prev = null;
+        while(root != null) {
+            prev = root;
+            root = root.right;
+        }
+        return prev;
+    }
+}   
+ 
+----------
+
+// Approach 2 - recursion and queue
+    
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
