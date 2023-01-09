8d53a7c5279d3a84ac40a2dc15d63876e44fb9b6
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2023-01-09 15:38:07 +0530

    Binary Tree, DFS, Recursive, Iterative

--- BinaryTree/144. Binary Tree Preorder Traversal.java
+++ BinaryTree/144. Binary Tree Preorder Traversal.java
@@ -14,6 +14,8 @@ Output: []
 
 */
 -------------------------
+  
+  // Preorder is root-left-right
 
 /**
  * Definition for a binary tree node.
@@ -49,4 +51,54 @@ class Solution {
     }
 }
 
-// Preorder is root-left-right
+
+=====================
+  
+// Approach - Iterative
+
+// Create an empty stack and an empty list to store the traversal.
+// Push the root node to the stack.
+// While the stack is not empty; Pop the top element from the stack.
+// Add the popped element's value to the traversal list.
+// Push the right child of the popped element to the stack.
+// Push the left child of the popped element to the stack.
+// Return the traversal list.
+
+// The reason why the right child is pushed to the stack before the left child is that we want to process the left subtree before the right subtree (since this is a pre-order traversal). Pushing the right child before the left child ensures that the left child will be processed first when it is popped from the stack.
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root == null)
+            return new ArrayList<>();
+        
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> s1 = new Stack();
+
+        s1.push(root);
+        while(!s1.isEmpty()) {
+            TreeNode current = s1.pop();
+            list.add(current.val);
+            if(current.right != null)
+                s1.add(current.right);
+            if(current.left != null)
+                s1.add(current.left);
+        }
+        return list;
+    }
+}
