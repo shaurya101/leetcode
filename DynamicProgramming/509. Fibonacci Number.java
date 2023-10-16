9e1e0ffef09a67e9423f376452a52ad1217d46ba
shaurya101 <71705373+shaurya101@users.noreply.github.com>
2023-10-16 18:58:42 +0530

    DP tabulation with space optimise, Recursion

--- DynamicProgramming/509. Fibonacci Number.java
+++ DynamicProgramming/509. Fibonacci Number.java
@@ -14,12 +14,70 @@ Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
 */
 -------------
 
+// Approach : T: O(N), S: O(1)
+// DP - tabulation with space optimisation
+    
 class Solution {
     public int fib(int n) {
-        if(n==0)
-            return 0;
-        if(n==1)
-            return 1;
+        // Base case: If n is 0 or 1, return n
+        if (n <= 1)
+            return n;
+
+        // Initialize variables to keep track of the previous two Fibonacci numbers
+        // We dont need dp array as we only need to track previous two fibonnaci numbers
+        int prev2 = 0; // Represents the Fibonacci number at index (i-2)
+        int prev = 1;  // Represents the Fibonacci number at index (i-1)
+
+        // Calculate Fibonacci numbers from 2 to 'n' using a bottom-up approach
+        for (int i = 2; i <= n; i++) {
+            // Calculate the Fibonacci number at index 'i' by adding the previous two numbers
+            int currI = prev + prev2; 
+
+            // Update the previous two numbers for the next iteration
+            prev2 = prev;
+            prev = currI;
+        }
+
+        // Return the Fibonacci number at index 'n'
+        return prev;
+    }
+}
+
+
+    
+-------------
+    
+// Approach : T: O(N), S: O(N)
+// DP - tabulation(bottom up approach)
+    
+class Solution {
+    public int fib(int n) {
+        // Base case: If n is 0 or 1, return n
+        if (n <= 1)
+            return n;
+
+        // Initialize an array to store Fibonacci numbers up to 'n'
+        int[] dp = new int[n + 1];
+        dp[0] = 0; // Fibonacci number at index 0 is 0
+        dp[1] = 1; // Fibonacci number at index 1 is 1
+
+        // Calculate Fibonacci numbers from 2 to 'n' using a bottom-up approach
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2]; // Calculate Fibonacci number at index 'i'
+        }
+
+        // Return the Fibonacci number at index 'n'
+        return dp[n];
+    }
+}
+
+
+-------------
+
+class Solution {
+    public int fib(int n) {
+        if(n<=0)
+            return n;
         return fib(n-1)+fib(n-2);
     }
 }
